230c4ec1a8e2b1025b7a17a1f93c3c32836e3d1f
colinlo-macbookair <locolin99@gmail.com>
2022-11-24 11:07:22 +0800

    update main loop

--- src/Main.java
+++ src/Main.java
@@ -6,63 +6,72 @@ public class Main {
     public static String dbUsername = "Group14";
     public static String dbPassword = "CSCI3170";
 
+    public static String[] user = {
+            "administrator",
+            "salesperson",
+            "manager"
+    };
+
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
         System.out.println("Connection Established successfully");
 
         Statement st = con.createStatement();
-//        st.executeUpdate("CREATE TABLE Sailors" +
-//                "(sid integer, name varchar(32)," +
-//                "rating integer, age float)");
-//        st.executeUpdate("INSERT INTO Sailors VALUES ( 22, 'Dustin', 7, 45.0 )");
-//        st.executeUpdate("INSERT INTO Sailors VALUES ( 25, 'Smith', 8, 50.0 )");
-//        String query = "SELECT sname, rating FROM Sailors";
-//        ResultSet rs = st.executeQuery(query);
-//
-//        while(rs.next()){
-//            String s = rs.getString("sname");
-//            int n = rs.getInt("rating");
-//            System.out.println(s + " " + n);
-//        }
 
+        loop:
+        while(true){
+            // print statements
+            System.out.println("-----Main menu-----");
+            System.out.println("What kinds of operation would you like to perform?");
+            for (int i = 0; i < 3; i++){
+                System.out.println((i+1) + ". Operations for " + user[i]);
+            }
+            System.out.println("4. Exit this program");
+            System.out.print("Enter Your Choice: ");
+            int choice = scanner.nextInt();
 
-        String[] user = {
-                "administrator",
-                "salesperson",
-                "manager"
-        };
-        System.out.println("-----Main menu-----");
-        System.out.println("What kinds of operation would you like to perform?");
-        for (int i = 0; i < 3; i++){
-            System.out.println((i+1) + ". Operations for " + user[i]);
-        }
-        System.out.println("4. Exit this program");
-        System.out.print("Enter Your Choice: ");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+            if(choice == 4){
+                System.exit(0);
+            }
+            System.out.println("-----Operations for " + user[choice-1] + " menu -----");
+            System.out.println("What kinds of operation would you like to perform?");
+            // admin
+            if (choice - 1 == 0){
+                Administrator admin = new Administrator(con);
+                admin.printOperations();
+                System.out.print("Enter Your Choice: ");
+                int opChoice = scanner.nextInt();
+                switch (opChoice) {
+                    case 1:
+                        admin.createTable();
+                        break;
+                    case 2:
+                        admin.deleteTable();
+                        break;
+                    case 3:
+                        admin.loadData();
+                        break;
+                    case 4:
+                        admin.showContent();
+                        break;
+                    case 5:
+                        continue loop;
+                }
+            }
+            // salesperson
+            else if (choice - 1 == 1){
+                Salesperson sales = new Salesperson();
 
-        if(choice == 4){
-            System.exit(0);
+            }
+            // manager
+            else {
+                System.out.println("Invalid Input");
+            }
         }
-        System.out.println("-----Operations for " + user[choice-1] + " menu -----");
-        System.out.println("What kinds of operation would you like to perform?");
 
-        // admin
-        if (choice - 1 == 0){
-            Administrator admin = new Administrator(st);
-            admin.printOperations();
-            admin.createTable();
-        }
-        // salesperson
-        else if (choice - 1 == 1){
-            Salesperson sales = new Salesperson();
-
-        }
-        // manager
-        else {
-            System.out.println("Invalid Input");
-        }
     }
 }
 
